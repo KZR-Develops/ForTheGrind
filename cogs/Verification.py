fc5da624c15cc95d8009e006e88fe47ec5593fd9
kzrdevelops@gmail.com <eaterdevelops@gmail.com>
2023-08-31 21:59:20 +0800

    Better Verification UI

--- cogs/Verification.py
+++ cogs/Verification.py
@@ -7,6 +7,17 @@ class Verification(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+
+        unverified_id = 1146684980563558440
+
+        if (member.bot):
+            return
+        
+        unverified = discord.utils.get(member.guild.roles, id=unverified_id)
+        await member.add_roles(unverified)
+        
     @commands.command()
     async def vsetup(self, ctx):
         embedVerification = discord.Embed(title="New here?", description="Click the verify button below to get verified.", color=0x5865f2)
--- views/Verify.py
+++ views/Verify.py
@@ -9,12 +9,18 @@ class Verify(discord.ui.View):
         
     @discord.ui.button(label="Verify", style=discord.ButtonStyle.blurple, custom_id="verify:blurple")
     async def verify(self, interaction: discord.Interaction, button: discord.Button):
-        verified_id = 1071779538725511178
+        unverified_id = 1146684980563558440
+        verified_id = 1145298592173662269
+        defaults_id = 1145298529183604778
+
         embedVerified = discord.Embed(description="Great! You have been verified.\n\nYou can now start talking to other members.", color=0x00ff00)
         channel = interaction.channel
         user = interaction.user
         verified = discord.utils.get(user.guild.roles, id=verified_id)
+        defaults = discord.utils.get(user.guild.roles, id=defaults_id)
+        unverified = discord.utils.get(user.guild.roles, id=unverified_id)
         
-        await interaction.response.send_message(embed=embedVerified, ephemeral=True)
-        await asyncio.sleep(1)
+        await user.remove_roles(unverified)
         await user.add_roles(verified)
+        await user.add_roles(defaults)
+        await interaction.response.send_message(embed=embedVerified, ephemeral=True)
