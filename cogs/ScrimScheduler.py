a3ada420e8e87c3acf910693b509d9a1db53bba1
kzrdevelops@gmail.com <eaterdevelops@gmail.com>
2023-11-03 23:00:10 +0800

    Added Checks and Counters

--- cogs/ScrimScheduler.py
+++ cogs/ScrimScheduler.py
@@ -9,12 +9,17 @@ class ScrimScheduler(commands.Cog):
     @commands.command()
     async def schedule(self, ctx, game, *, text):
         communityChannel = ctx.guild.get_channel(1134737529443196988)
-        description = f"Game: {game}\n{text}"
+        if game == "Valorant":
+            description = f"Game: Valorant\n{text}"
+        if game == "CODM":
+            description = f"Game: Call of Duty: Mobile\n{text}"
+        if game == "ML":
+            description = f"Game: Mobile Legends: Bang Bang\n{text}"
 
         scheduleEmbed = discord.Embed(description=description, color=0xB50000)
         scheduleEmbed.set_author(name="Community Scrim Manager")
-        scheduleEmbed.add_field(name="Joined",  value="No players", inline=True)
-        scheduleEmbed.add_field(name="Tentative", value="No players", inline=True)
+        scheduleEmbed.add_field(name="Joined 0/5",  value="No players yet", inline=True)
+        scheduleEmbed.add_field(name="Tentative 0/5", value="No players yet", inline=True)
         # Create the message with the view and send it
         message = await communityChannel.send(embed=scheduleEmbed, view=ParticipantsControl(ctx=ctx, game=game, text=description))
 
@@ -38,20 +43,28 @@ class ParticipantsControl(discord.ui.View):
                 self.tentativePlayers.remove(user_id)
             if user_id not in self.joinedPlayers:  # Check if the user is not already in the list
                 self.joinedPlayers.append(user_id)
-                print(f"A participant has joined. Total Players Joined: {len(self.joinedPlayers)}/5")
                 if self.game == "Valorant":
                     ScrimChannel = self.ctx.guild.get_channel(1160519497711628370)
                     role = discord.utils.get(interaction.user.guild.roles, id=1160522888554741772)
                     await interaction.user.add_roles(role)
+                if self.game == "CODM":
+                    ScrimChannel = self.ctx.guild.get_channel(1160519716201300029)
+                    role = discord.utils.get(interaction.user.guild.roles, id=1160523029009412136)
+                    await interaction.user.add_roles(role)
+                if self.game == "ML":
+                    ScrimChannel = self.ctx.guild.get_channel(1160519812313792574)
+                    role = discord.utils.get(interaction.user.guild.roles, id=1160523062802907167)
+                    await interaction.user.add_roles(role)
+                
 
                 joinedEmbed = discord.Embed(description=f"You have successfully joined the scrim!\nYou can now access {ScrimChannel.mention}.")
                 scheduleEmbed = discord.Embed(description=self.text, color=0xb50000)
                 scheduleEmbed.set_author(name="Community Scrim Manager")
-                scheduleEmbed.add_field(name="Joined", value="\n".join([f"<:B1:1134737275318706278><@{player}>" for player in self.joinedPlayers]))
+                scheduleEmbed.add_field(name=f"Joined {len(self.joinedPlayers)}/5", value="\n".join([f"<:B1:1134737275318706278><@{player}>" for player in self.joinedPlayers]))
                 if self.tentativePlayers:
-                    scheduleEmbed.add_field(name="Tentative", value="\n".join([f"<:B1:1134737275318706278><@{player}>" for player in self.tentativePlayers]))
+                    scheduleEmbed.add_field(name=f"Tentative {len(self.tentativePlayers)}/5", value="\n".join([f"<:B1:1134737275318706278><@{player}>" for player in self.tentativePlayers]))
                 else:
-                    scheduleEmbed.add_field(name="Tentative", value="No players yet")
+                    scheduleEmbed.add_field(name="Tentative 0/5", value="No players yet")
                 
                 await interaction.message.edit(embed=scheduleEmbed)
                 await interaction.response.send_message(embed=joinedEmbed, ephemeral=True)
@@ -62,6 +75,19 @@ class ParticipantsControl(discord.ui.View):
                 )
 
                 await interaction.response.send_message(embed=already, ephemeral=True)
+        else:
+            if len(self.tentativePlayers) < 5:
+                already = discord.Embed(
+                    description="The main party is already full, why not try to fill in the slot for the subparty?",
+                    color=0xb50000
+                )
+            else:
+                already = discord.Embed(
+                    description="This game is already full. Try again next time!",
+                    color=0xb50000
+                )
+
+            await interaction.response.send_message(embed=already, ephemeral=True)
 
     @discord.ui.button(
         label="Join Tentative", style=discord.ButtonStyle.grey, custom_id="pc:JT:gray"
@@ -86,10 +112,10 @@ class ParticipantsControl(discord.ui.View):
                 scheduleEmbed = discord.Embed(description=self.text, color=0xb50000)
                 scheduleEmbed.set_author(name="Community Scrim Manager")
                 if self.joinedPlayers:
-                    scheduleEmbed.add_field(name="Joined", value="\n".join([f"<:B1:1134737275318706278><@{player}>" for player in self.joinedPlayers]))
+                    scheduleEmbed.add_field(name=f"Joined {len(self.joinedPlayers)}/5", value="\n".join([f"<:B1:1134737275318706278><@{player}>" for player in self.joinedPlayers]))
                 else: 
-                    scheduleEmbed.add_field(name="Joined", value="No players yet")
-                scheduleEmbed.add_field(name="Tentative", value="\n".join([f"<:B1:1134737275318706278><@{player}>" for player in self.tentativePlayers]))
+                    scheduleEmbed.add_field(name="Joined 0/5", value="No players yet")
+                scheduleEmbed.add_field(name=f"Tentative {len(self.tentativePlayers)}/5", value="\n".join([f"<:B1:1134737275318706278><@{player}>" for player in self.tentativePlayers]))
                 
                 await interaction.message.edit(embed=scheduleEmbed)
                 await interaction.response.send_message(embed=tentativeEmbed, ephemeral=True)
@@ -100,6 +126,64 @@ class ParticipantsControl(discord.ui.View):
                 )
 
                 await interaction.response.send_message(embed=already, ephemeral=True)
+        else:
+            if len(self.joinedPlayers) < 5:
+                already = discord.Embed(
+                    description="The sub-party is already full.",
+                    color=0xb50000
+                )
+            else:
+                already = discord.Embed(
+                    description="This game is already full. Try again next time!",
+                    color=0xb50000
+                )
+
+            await interaction.response.send_message(embed=already, ephemeral=True)
+
+    @discord.ui.button(label="Leave Party", style=discord.ButtonStyle.grey, custom_id="pc:LP:gray")
+    async def leave(self, interaction: discord.Interaction, button: discord.Button):
+        if interaction.user.id in self.joinedPlayers:
+            joinedRole = discord.utils.get(interaction.guild.roles, id=1160522888554741772)
+            if joinedRole is not None:
+                await interaction.user.remove_roles(joinedRole)
+                embedSuccess = discord.Embed(
+                    description="You have left the party."
+                )
+                self.joinedPlayers.remove(interaction.user.id)
+
+                await interaction.response.send_message(embed=embedSuccess, ephemeral=True)
+            else:
+                print("Role not found.")
+
+        elif interaction.user.id in self.tentativePlayers:
+            embedSuccess = discord.Embed(
+                    description="You have left the sub-party."
+                )
+            self.tentativePlayers.remove(interaction.user.id)
+
+            await interaction.response.send_message(embed=embedSuccess, ephemeral=True)
+
+        else:
+            embedError = discord.Embed(
+                    description="You are not on the list of players."
+                )
+
+            await interaction.response.send_message(embed=embedError, ephemeral=True)
+
+            
+        scheduleEmbed = discord.Embed(description=self.text, color=0xb50000)
+        scheduleEmbed.set_author(name="Community Scrim Manager")
+        if self.joinedPlayers:
+            scheduleEmbed.add_field(name=f"Joined {len(self.joinedPlayers)}/5", value="\n".join([f"<:B1:1134737275318706278><@{player}>" for player in self.joinedPlayers]))
+        else: 
+            scheduleEmbed.add_field(name="Joined 0/5", value="No players yet")
+        if self.tentativePlayers:
+            scheduleEmbed.add_field(name=f"Tentative {len(self.tentativePlayers)}/5", value="\n".join([f"<:B1:1134737275318706278><@{player}>" for player in self.tentativePlayers]))
+        else:
+            scheduleEmbed.add_field(name="Tentative 0/5", value="No players yet")
+
+        
+        await interaction.message.edit(embed=scheduleEmbed)
 
 
 async def setup(bot):
