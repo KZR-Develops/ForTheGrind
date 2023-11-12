8df4e9aef0a65a66e636ab98c1c67054aaf46f50
kzrdevelops@gmail.com <eaterdevelops@gmail.com>
2023-11-12 17:56:42 +0800

    switched back to terminal outputs

--- start_bot.bat
+++ start_bot.bat
@@ -1,20 +1,10 @@
 @echo off
 chcp 65001
+title ForTheGrind - Discord Server Bot
 call c:/Life/Programming/ForTheGrindBot/.venv/Scripts/activate
-
-REM Create a timestamp for the log file name (format: YYYYMMDD_HHMMSS)
-for /f "tokens=2 delims==." %%I in ('wmic os get LocalDateTime /value') do set "timestamp=%%I"
 
 REM Set the log folder and the number of log files to keep
 set "logFolder=C:\Life\Programming\ForTheGrindBot\logs"
-set "logsToKeep=5"
 
-REM Delete old log files, keeping the newest 'logsToKeep' logs
-for /f "tokens=1,* delims=_" %%F in ('dir /b /o-n "%logFolder%\output_*.log"') do (
-    set /a "count+=1"
-    if !count! gtr %logsToKeep% (
-        del "%logFolder%\%%F_%%G"
-    )
-)
-
-python "C:/Life/Programming/ForTheGrindBot/bot.py" > "%logFolder%\output_%timestamp%.log" 2>&1
+REM Run your Python script and redirect the output to a log file
+python "C:/Life/Programming/ForTheGrindBot/bot.py"
